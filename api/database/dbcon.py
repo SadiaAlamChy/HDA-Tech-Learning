c4a801a7fbd197626975d70ec497aabd7b397512
Afra Binth Osman <40483069+binthafra@users.noreply.github.com>
2022-02-27 08:40:16 +0600

    connect database

---
+++ api/database/dbcon.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+
+class PostgresConnection(object):
+    def __init__(self):
+        self.connection = psycopg2.connect(database="ecomdb",
+                                           user="postgres",
+                                           password="afra1234",
+                                           host="127.0.0.1",
+                                           port="5432")
+
+    def getConnection(self):
+        print("successfully connected to database")
+        return self.connection
+        
+conn = PostgresConnection().getConnection()
