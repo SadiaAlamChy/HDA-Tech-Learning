0deb073b910cae58fd3bc48281ce186becd4e053
tanvirkse101 <tanvirkse@gmail.com>
2022-03-01 20:00:01 +0600

    updated queries

--- api/controller/q10.py
+++ api/controller/q10.py
@@ -1,6 +1,10 @@
 from db.postgres import PostgresConnection
 import pandas as pd
 
+months = ['January', 'February', 'March', 'April',
+          'May', 'June', 'July', 'August', 'September',
+          'October', 'November', 'December']
+
 
 class Query10:
     def __init__(self):
@@ -10,17 +14,32 @@ class Query10:
     def execute():
         con = PostgresConnection().get_connection()
         cur = con.cursor()
-        query = "select s.store_key, t.month, round(avg(f.total_price)::numeric,2) " \
+        query = "select s.store_key, i.item_key, i.item_name, t.month, sum(f.total_price) " \
                 "from star_schema.fact_table f " \
                 "join star_schema.store_dim s on s.store_key = f.store_key " \
                 "join star_schema.time_dim t on t.time_key = f.time_key " \
-                "group by s.store_key, t.month " \
-                "order by s.store_key, t.month"
+                "join star_schema.item_dim i on i.item_key = f.item_key " \
+                "group by s.store_key, i.item_key, i.item_name, t.month " \
+                "order by s.store_key, i.item_key, i.item_name, t.month " \
+                "limit 20000"
         cur.execute(query)
         result = cur.fetchall()
-        pd_data = pd.DataFrame(list(result), columns=['Store', 'Month', 'Avg'])
-        pd_data['Avg'] = pd_data['Avg'].astype('float64')
-        return pd_data.to_dict(orient='records')
+        pd_data = pd.DataFrame(list(result), columns=['store', 'item_key', 'item_name', 'month', 'sum'])
+        pd_data['sum'] = pd_data['sum'].astype('float64')
+        stores = pd_data['store'].unique()
+        store_list = []
+        for store in stores:
+            item_list = []
+            all_items = pd_data[pd_data['store'] == store]
+            unique_items = all_items['item_name'].unique()
+            for u in unique_items:
+                sales = []
+                rows = pd_data[(pd_data['store'] == store) & (pd_data['item_name'] == u)]
+                for i, j in rows.iterrows():
+                    sales.append({'month': months[int(j['month']) - 1], 'total_sales': j['sum']})
+                item_list.append({'item': u, 'sales': sales})
+            store_list.append({'store': store, 'items': item_list})
+        return store_list
 
 
 if __name__ == '__main__':
--- api/controller/q9.py
+++ api/controller/q9.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+
 from db.postgres import PostgresConnection
 import pandas as pd
 
@@ -10,18 +12,25 @@ class Query9:
     def execute():
         con = PostgresConnection().get_connection()
         cur = con.cursor()
-        query = "select i.item_name, s.division, sum(f.total_price) " \
+        query = "select i.item_key, i.item_name, s.division, sum(f.total_price) " \
                 "from star_schema.fact_table f " \
                 "join star_schema.item_dim i on i.item_key=f.item_key " \
                 "join star_schema.store_dim s on s.store_key = f.store_key " \
-                "where i.item_key ='I00082' " \
-                "group by i.item_name, s.division " \
-                "order by i.item_name,s.division"
+                "group by i.item_key, i.item_name, s.division " \
+                "order by i.item_key, i.item_name, s.division"
         cur.execute(query)
         result = cur.fetchall()
-        pd_data = pd.DataFrame(list(result), columns=['Item', 'Division', 'Sales'])
-        pd_data['Sales'] = pd_data['Sales'].astype('float64')
-        return pd_data.to_dict(orient='records')
+        pd_data = pd.DataFrame(list(result), columns=['key', 'item', 'division', 'sales'])
+        pd_data['sales'] = pd_data['sales'].astype('float64')
+        unique_items = pd_data['item'].unique()
+        nested_list = []
+        for u in unique_items:
+            div_sales = []
+            rows = pd_data[pd_data['item'] == u]
+            for i, j in rows.iterrows():
+                div_sales.append({'division': j['division'], 'total_sales': j['sales']})
+            nested_list.append({'item': u, 'sales': div_sales})
+        return nested_list
 
 
 if __name__ == '__main__':
